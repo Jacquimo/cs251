c69ff12a511649d20c25e5a2a00d4201bc18f19a
Gray Houston <ghousto@purdue.edu>
2014-09-22 19:38:58 -0400

    Implemented partition function in quicksort

--- project2_skeleton/src/Quick.java
+++ project2_skeleton/src/Quick.java
@@ -1,12 +1,14 @@
+import java.util.Random;
+
 
 /**
  * General Quick Sort
  * 
- * TODO if this is an off-the-shelf implementation, cite where you got it from.
+ * TODO This implementation uses the same logic as the one in the lecture slides
  * 
- * @author TODO put your username here
- * @version TODO put the date here
- * @pso   TODO put your PSO section here
+ * @author ghousto
+ * @version 9/22/14
+ * @pso   P06
  *
  */
 public class Quick extends Sort {
@@ -21,5 +23,37 @@ public class Quick extends Sort {
 	 */
 	public static void sort(Comparable[] a) {
 		// TODO implement sort
+	}
+	
+	public static void quicksort() {
+		
+	}
+	
+	public static int partition(Comparable[] a, int left, int right) {
+		// Randomly generate the pivot
+		Random generator = new Random();
+		int pivot = generator.nextInt(right - left + 1) + left;
+		Sort.exch(a, pivot, right);
+		
+		// Partition based on pivot value
+		int i = left, j = right-1;
+		Comparable pivotVal = a[right];
+		
+		while (true) {
+			// Find index i such that a[i] > a[right]
+			while (i < right && i < j && Sort.less(a[i], a[right]))
+				++i;
+			
+			// Find index j such that a[j] < a[right]
+			while (j >= left && i < j && Sort.less(a[right], a[j]))
+				--j;
+			
+			if (i >= j) break; // No more elements need to be swapped
+			Sort.exch(a, i, j);
+		}
+		// Need to make sure that j is the correct index to swap the pivot back into
+		Sort.exch(a, j, right);
+		
+		return j;
 	}
 }
