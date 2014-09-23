c922321c58cb3b16fc2723e28737ce579bb05dd8
Gray Houston <ghousto@purdue.edu>
2014-09-22 23:50:45 -0400

    Implemented Heapsort and it works. Need to add optimizations

--- project2_skeleton/src/Heap.java
+++ project2_skeleton/src/Heap.java
@@ -1,12 +1,14 @@
+import java.util.Arrays;
+
 
 /**
  * General Heap Sort
  * 
  * TODO if this is an off-the-shelf implementation, cite where you got it from.
  * 
- * @author TODO put your username here
- * @version TODO put the date here
- * @pso   TODO put your PSO section here
+ * @author ghousto
+ * @version 9/22/14
+ * @pso   P06
  *
  */
 public class Heap extends Sort {
@@ -20,6 +22,63 @@ public class Heap extends Sort {
 	 * @param a - array
 	 */
 	public static void sort(Comparable[] a) {
-		// TODO implement sort
+		Comparable[] extra = Arrays.copyOf(a, a.length);
+		buildHeap(extra);
+		for (int i = extra.length - 1; i >= 0; --i) {
+			a[i] = delMax(extra, i);
+		}
+	}
+	
+	public static void insertionSort(Comparable[] a, int left, int right) {
+		for (int i = left; i <= right; ++i) {
+			int j = i;
+			while (j > left && Sort.less(a[j], a[j-1])) {
+				Sort.exch(a, j, j-1);
+				--j;
+			}
+		}
+	}
+	
+	public static void buildHeap(Comparable[] a) {
+		for (int k = a.length / 2; k >= 0; --k) {
+			sink(a, k);
+		}
+	}
+	
+	public static Comparable delMax(Comparable[] a, int lastIndex) {
+		Comparable ret = a[0];
+		a[0] = a[lastIndex];
+		a[lastIndex] = null;
+		sink(a, 0);
+		
+		return ret;
+	}
+	
+	// a pre-condition for this method is that it will never be called on a leaf. Otherwise, there will likely be an exception
+	public static void sink(Comparable[] a, int k) {
+		if (!canHaveChildren(a,k)) return;
+		
+		Comparable left = a[2*k+1];
+		Comparable right = 2*k+2 < a.length ? a[2*k+2] : null;
+		
+		Comparable largerChild;
+		boolean leftIsLarger = false;;
+		if (left == null) {largerChild = right; leftIsLarger = false; }
+		else if (right == null) {largerChild = left; leftIsLarger = true; }
+		//else if (left == null && right == null) largerChild = null;
+		else {
+			largerChild = Sort.less(left, right) ? right : left;
+			leftIsLarger = Sort.equal(largerChild, left);
+		}
+		
+		if (largerChild != null && Sort.less(a[k], largerChild)) { // if a[k] is less than its larger child, keep sinking
+			Sort.exch(a, k, leftIsLarger ? 2*k+1 : 2*k+2);
+			if (canHaveChildren(a, leftIsLarger ? 2*k+1 : 2*k+2))
+				sink(a, leftIsLarger ? 2*k+1 : 2*k+2);
+		}
+	}
+	
+	public static boolean canHaveChildren(Comparable[] a, int k) {
+		return 2*k+1 < a.length;
 	}
 }
