509533027fa8fbc4259f20a7932220da5dc1bd6f
Gray Houston <ghousto@purdue.edu>
2014-09-25 14:57:17 -0400

    Removed previously commited mergesort optimization because it will hurt when the locality is greater than 5

--- project2_skeleton/src/Merge.java
+++ project2_skeleton/src/Merge.java
@@ -42,8 +42,6 @@ public class Merge extends Sort {
 		int middle = (left + right) / 2;
 		mergesort(a, aux, left, middle);
 		mergesort(a, aux, middle+1, right);
-		if (Sort.less(a[middle], a[middle + 1])) // if the largest element in the left subarray is <= smallest element in right subarray, they're already sorted
-			return;
 		merge(a, aux, left, middle, right);
 	}
 	
