a27679aa573f09feabbb9bb845b7373edae37928
Gray Houston <ghousto@purdue.edu>
2014-10-06 03:16:10 -0400

    Starting to use runAll. Currently running Selection Sort

--- project2_skeleton/runAll.sh
+++ project2_skeleton/runAll.sh
@@ -5,8 +5,8 @@ for d in 5 15 25 35 45
 do
 ############################################
 # use 10^5 data to test selection sort     #
-#printf -v data "data/10^5/L%ddata.txt" $d #
-printf -v data "data/10^6/L%ddata.txt" $d  #
+printf -v data "data/10^5/L%ddata.txt" $d #
+#printf -v data "data/10^6/L%ddata.txt" $d  #
 ############################################
 echo L = $d
 for r in 1 2 3 4 5
@@ -15,26 +15,26 @@ printf $d, >> runtimes.txt
 #######################################################################
 # comment out these two lines if you are running on 10^6 or more data #
 echo Selection run $r                                                 #
-java -cp bin Sort -a selection -f $data >> runtimes.txt               #
+java -cp .:src  Sort -a selection -f $data >> runtimes.txt               #
 #######################################################################
-printf , >> runtimes.txt
-echo LSelection run $r
-java -cp bin Sort -a selection -f $data -l $d >> runtimes.txt
-printf , >> runtimes.txt
-echo Merge run $r
-java -cp bin Sort -a merge -f $data >> runtimes.txt
-printf , >> runtimes.txt
-echo LMerge run $r
-java -cp bin Sort -a merge -f $data -l $d >> runtimes.txt
-printf , >> runtimes.txt
-echo Heap run $r
-java -cp bin Sort -a heap -f $data >> runtimes.txt
-printf , >> runtimes.txt
-echo LHeap run $r
-java -cp bin Sort -a heap -f $data -l $d >> runtimes.txt
-printf $'\r\n' >> runtimes.txt
-echo Quick run $r
-java -cp bin Sort -a quick -f $data >> runtimes.txt
-printf , >> runtimes.txt
+#printf , >> runtimes.txt
+#echo LSelection run $r
+#java -cp bin Sort -a selection -f $data -l $d >> runtimes.txt
+#printf , >> runtimes.txt
+#echo Merge run $r
+#java -cp bin Sort -a merge -f $data >> runtimes.txt
+#printf , >> runtimes.txt
+#echo LMerge run $r
+#java -cp bin Sort -a merge -f $data -l $d >> runtimes.txt
+#printf , >> runtimes.txt
+#echo Heap run $r
+#java -cp bin Sort -a heap -f $data >> runtimes.txt
+#printf , >> runtimes.txt
+#echo LHeap run $r
+#java -cp bin Sort -a heap -f $data -l $d >> runtimes.txt
+#printf $'\r\n' >> runtimes.txt
+#echo Quick run $r
+#java -cp bin Sort -a quick -f $data >> runtimes.txt
+#printf , >> runtimes.txt
 done
 done
