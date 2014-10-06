0b1c9ebe381becec109eb9e28f9562f6c496f418
Gray Houston <ghousto@purdue.edu>
2014-10-06 19:59:28 -0400

    Ran new data and answered more analysis questions

--- project2_skeleton/makeData.sh
+++ project2_skeleton/makeData.sh
@@ -10,43 +10,52 @@ do
     printf -v dataFile $'%sdata.txt' $alg 
     rm $dataFile
 
-    for locality in 1 2
+    printf $'L%s\n' $alg >> $dataFile
+    printf $'Size,Locality,First Run,Second Run,Third Run,Fourth Run,Fifth Run,Average Run Time' >> $dataFile
+
+    for size in 3 5 6
     do
-	if [ "$locality" = "1" ] ; then
-	    printf $'%s\n' $alg >> $dataFile
-	else
-	    if [ "$alg" = "quick" ] ; then
-		break
-	    fi
-	    printf $'L%s\n' $alg >> $dataFile
-	fi
 
-	printf $'Size,Locality,First Run,Second Run,Third Run,Fourth Run,Fifth Run,Average Run Time\n' >> $dataFile
-
-	for d in 5 15 25 35 45
-	do
-	    size=6
-	    if [ "$alg" = "selection" ] ; then
-		printf -v data "data/10^5/L%ddata.txt" $d
-		size=5
-	    else
-		printf -v data "data/10^6/L%ddata.txt" $d
-	    fi
-
-	    printf $'10^%d,%d,' $size $d  >> $dataFile
-
-	    for run in 1 2 3 4 5
+	#for locality in 2
+	#do
+	    #if [ "$locality" = "1" ] ; then
+		#printf $'%s\n' $alg >> $dataFile
+	    #else
+		#if [ "$alg" = "quick" ] ; then
+		    #break
+		#fi
+		#printf $'L%s\n' $alg >> $dataFile
+	    #fi
+	    
+	    #printf $'Size,Locality,First Run,Second Run,Third Run,Fourth Run,Fifth Run,Average Run Time\n' >> $dataFile
+	    
+	    for d in 5 15 25 35 45
 	    do
-		if [ "$locality" = "1" ] ; then
-		    java -cp .:src Sort -a $alg -f $data >> $dataFile
+		#size=6
+		if [ "$alg" = "selection" ] ; then
+		    printf -v data "data/10^%d/L%ddata.txt" $size  $d
+		    #size=5
 		else
-		    java -cp .:src Sort -a $alg -f $data -l $d >> $dataFile
+		    printf -v data "data/10^%d/L%ddata.txt" $size  $d
 		fi
-		printf , >> $dataFile
+		
+		printf $'10^%d,%d,' $size $d  >> $dataFile
+		
+		for run in 1 2 3 4 5
+		do
+		    #if [ "$locality" = "1" ] ; then
+			#java -cp .:src Sort -a $alg -f $data >> $dataFile
+		    #else
+			java -cp .:src Sort -a $alg -f $data -l $d >> $dataFile
+		    #fi
+		    printf , >> $dataFile
+		done
+		printf $'\n' >> $dataFile
+		
 	    done
-	    printf $'\n' >> $dataFile
-
-	done
-	printf $'\n\n' >> $dataFile
+	    #printf $'\n' >> $dataFile
+	#done
+	printf $'\n' >> $dataFile
     done
+    printf $'\n\n' >> $dataFile
 done
