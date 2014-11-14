07f63233bce0cc0106fbdd7ef095adb5e85d99b5
Gray Houston <ghousto@purdue.edu>
2014-11-14 16:10:37 -0500

    Started workin on parsing in the tree. So far, takes the size of the tree

--- Project4/src/part1/Tree.java
+++ Project4/src/part1/Tree.java
@@ -1,15 +1,25 @@
 package part1;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * 
- * @author TODO username
- * @version TODO date
+ * @author ghousto
+ * @version 11/14/14
  *
  */
 public class Tree {
-    // TODO
+    
+	private ArrayList<Node> adjacencyLS;
+	private Node root;
+	
+	public Tree(int numOfNodes) {
+		adjacencyLS = new ArrayList<Node>(numOfNodes);
+	}
+	
     /**
      * compute the size of this tree
      * @return number of nodes in tree
@@ -18,6 +28,22 @@ public class Tree {
         //TODO add your code here
         return 0;
     }
+    
+    /**
+     * Setter method for root field
+     * @param node
+     */
+    public void setRoot(Node node) {
+    	this.root = node;
+    }
+    
+    /**
+     * Getter method for root field
+     * @return
+     */
+    public Node getRoot() {
+    	return this.root;
+    }
 
     /**
      * read a rooted tree from file
@@ -25,7 +51,22 @@ public class Tree {
      * @return tree rooted at node 0
      */
     public static Tree readTreeFromFile(File f) {
-        //TODO add your code her
+    	Scanner reader = null;
+    	try {
+			reader = new Scanner(f);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	// Parse first line (i.e. the size of the tree)
+    	if (reader.hasNextLine()) {
+    		String size = reader.nextLine();
+    		return new Tree(Integer.parseInt(size));
+    	}
+    	
+    	// Parse all nodes and put their connections into the 
+        
         return null;
     }
     /**
