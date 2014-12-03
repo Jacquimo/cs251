0b1dfd4eae078e60ca77df6537a3a0a1bb4e9c6c
Gray Houston <ghousto@purdue.edu>
2014-12-03 15:46:27 -0500

    Added the skeleton code

---
+++ 5-project/project5_skeleton/src/DagUtilities.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class DagUtilities {
+
+    /** Task 1
+     * This function performs a toplogical sort on a graph
+     * 
+     * @param dag G
+     * @return an array containing the topological numbers of each vertex in graph G
+     */
+    public static int[] topologicalSort(Digraph G) {
+        // TODO add your code here
+        return null;
+    }
+
+    /**
+     * Task 2
+     * This function determines the length of the longest path
+     * 
+     * @param dag G
+     * @return length of the longest path
+     */
+    public static int longestPath(Digraph G) {
+        return -1;
+    }
+
+    /**
+     * Task 3
+     * This fundtion  generates a schedule that completes all manufacturing steps in the shortest
+     * production span.
+     * 
+     * @param dag G
+     * @return a schedule
+     */
+    public static Schedule minProdSpanSchedule(Digraph G) {
+        //TODO add your code here
+        return null;
+    }
+
+    /**
+     * Task 4
+     * This function generates a schedule using at most k stations. 
+     * @param dag G
+     * @param the number of stations k
+     * @return a schedule
+     */
+    public static Schedule spanKStations(Digraph G, int k) {
+        //TODO add your code here
+        return null;
+    }
+
+    /**
+     * This function reads a graph from an input file
+     * 
+     * @param file
+     * @return dag G
+     */
+    public static Digraph readGraphFromFile(File file) {
+        // TODO add you code here
+        return null;
+    }
+
+}
---
+++ 5-project/project5_skeleton/src/Digraph.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+/**
+ * 
+ * This is the graph structure
+ * TODO: You should implement it
+ *
+ */
+public class Digraph
+{
+
+}
